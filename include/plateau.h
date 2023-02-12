2b1c7553be301edde70bd58ee0b71db3798a8b49
AlexandreMarcq <alexandremarcq58@gmail.com>
2023-02-12 19:04:12 +0100

    Ajout du plateau

--- include/plateau.h
+++ include/plateau.h
@@ -1,7 +1,10 @@
 #ifndef PLATEAU_H
 #define PLATEAU_H
 
-#include <pions.h>
+#include <stdlib.h>
+#include "joueur.h"
+#include "paquet.h"
+#include "pion.h"
 
 typedef struct {
     Pions* dix_carreau;
@@ -9,6 +12,11 @@ typedef struct {
     Pions* dame_pique;
     Pions* roi_coeur;
     Pions* sept_carreau;
+    Paquet* paquet;
+    ListeJoueur* joueurs;
 } Plateau;
+
+Plateau* nouveau_plateau(size_t nombre_joueurs);
+void supprimer_plateau(Plateau* p);
 
 #endif
---
+++ src/plateau.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "plateau.h"
+
+Plateau*
+nouveau_plateau(size_t nombre_joueurs)
+{
+    Plateau* plateau = (Plateau*) malloc(sizeof(Plateau));
+
+    Paquet* paquet = (Paquet*) malloc(sizeof(Paquet));
+    paquet->cartes = NULL;
+    generer_paquet(paquet);
+    melanger_paquet(paquet);
+
+    plateau->joueurs = NULL;
+    for (int i = 0; i < nombre_joueurs; i++)
+    {
+        Joueur* j = nouveau_joueur();
+        ajouter_joueur(&(plateau->joueurs), j);
+    }
+
+    plateau->paquet = paquet;
+    return plateau;
+}
+
+void
+supprimer_plateau(Plateau* p)
+{
+    supprimer_paquet(p->paquet);
+    vider_liste_joueur(p->joueurs);
+    free(p);
+}
