df51a9928b05fee31b8b3df95b5ef66602febc60
AlexandreMarcq <alexandremarcq58@gmail.com>
2023-02-12 18:51:35 +0100

    Ajout de la liste des joueurs

--- include/joueur.h
+++ include/joueur.h
@@ -9,7 +9,15 @@ typedef struct {
     Pions* pions;
 } Joueur;
 
+typedef struct ListeJoueur {
+    Joueur* joueur;
+    struct ListeJoueur* suivant;
+} ListeJoueur;
+
+Joueur* nouveau_joueur();
 void initialiser_pions_joueur(Joueur* j);
 void supprimer_joueur(Joueur* j);
+void ajouter_joueur(ListeJoueur** premier, Joueur* j);
+void vider_liste_joueur(ListeJoueur* premier);
 
 #endif
--- src/joueur.c
+++ src/joueur.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
 #include "joueur.h"
 
+Joueur*
+nouveau_joueur()
+{
+    Joueur* j = (Joueur*) malloc(sizeof(Joueur));
+    j->main = NULL;
+    initialiser_pions_joueur(j);
+    return j;
+}
+
 void
 initialiser_pions_joueur(Joueur* j)
 {
@@ -15,5 +24,43 @@ void
 supprimer_joueur(Joueur* j)
 {
     free(j->pions);
+    vider_liste_carte(j->main);
     free(j);
+}
+
+void
+ajouter_joueur(ListeJoueur** premier, Joueur* j)
+{
+    ListeJoueur* nouveau = (ListeJoueur*) malloc(sizeof(ListeJoueur));
+    nouveau->joueur = j;
+    nouveau->suivant = NULL;
+
+    if (*premier == NULL)
+    {
+        *premier = nouveau;
+        return;
+    }
+
+    ListeJoueur* temp = *premier;
+    while (temp->suivant != NULL)
+    {
+        temp = temp->suivant;
+    }
+
+    temp->suivant = nouveau;
+    return;
+}
+
+void
+vider_liste_joueur(ListeJoueur* premier)
+{
+    ListeJoueur* temp;
+
+    while (premier != NULL)
+    {
+        temp = premier;
+        premier = premier->suivant;
+        supprimer_joueur(temp->joueur);
+        free(temp);
+    }
 }
