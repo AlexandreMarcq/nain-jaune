16cd2f2f855ede9237e6710d175b8e3dd5aa29ed
AlexandreMarcq <alexandremarcq58@gmail.com>
2023-02-12 19:03:27 +0100

    Affichage d'un pion

--- include/pion.h
+++ include/pion.h
@@ -7,4 +7,6 @@ typedef struct {
     int nombre_dix;
 } Pions;
 
+void afficher_pions(Pions p);
+
 #endif
---
+++ src/pion.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "pion.h"
+
+void
+afficher_pions(Pions p)
+{
+    printf("1: %d, 5: %d, 10: %d\n", p.nombre_un, p.nombre_cinq, p.nombre_dix);
+}
